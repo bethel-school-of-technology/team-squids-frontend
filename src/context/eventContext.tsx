d7f6dbb6397b484f7a88355b61bc699a7ce1d0c7
Jeff Ordway <jeffordway@Jeffs-iMac.local>
2023-05-23 10:17:13 -0700

    added CRUD operations for eventContext

--- src/context/eventContext.tsx
+++ src/context/eventContext.tsx
@@ -1,11 +1,157 @@
+import axios from "axios";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+
 export interface Event {
-   eventId: number;
-   eventTitle: string;
-   churchName: string;
-   eventDay: string;
-   eventDate: string;
-   eventTime: string;
-   eventAddress: string;
-   eventType: "Family" | "Youth" | "Young Adults" | "Single" | "Senior";
-   description: string;
+  eventId: number;
+  churchId: number;
+  eventTitle: string;
+  eventDate: Date;
+  eventDay: string;
+  eventTime: string;
+  eventStreet: string;
+  eventCity: string;
+  eventState: string;
+  eventZip: string;
+  eventType:
+    | "Family"
+    | "Youth"
+    | "Young Adults"
+    | "Single"
+    | "Womans"
+    | "Mens"
+    | "Senior";
+  description: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
+
+export interface newEvent {
+  churchId: number;
+  eventTitle: string;
+  eventDate: Date;
+  eventDay: string;
+  eventTime: string;
+  eventStreet: string;
+  eventCity: string;
+  eventState: string;
+  eventZip: string;
+  eventType:
+    | "Family"
+    | "Youth"
+    | "Young Adults"
+    | "Single"
+    | "Womans"
+    | "Mens"
+    | "Senior";
+  description: string;
+}
+
+interface EventContextProps {
+  events: Event[];
+  setEvents: Dispatch<SetStateAction<Event[]>>;
+  getAllEvents: () => Promise<void>;
+  createEvent: (newEvent: newEvent) => Promise<newEvent>;
+  getEvent: (eventId: number) => Promise<Event>;
+  updateEvent: (updatedEvent: Event) => Promise<Event>;
+  deleteEvent: (eventId: number) => Promise<Event>;
+}
+
+interface EventContextProviderProps {
+  children: ReactNode;
+}
+
+export const EventContext = createContext<EventContextProps>({
+  events: [],
+  setEvents: () => {},
+  getAllEvents: () => Promise.resolve(),
+  createEvent: (newEvent: newEvent) => Promise.resolve(newEvent),
+  getEvent: (eventId: number) => Promise.resolve({} as Event),
+  updateEvent: (updatedEvent: Event) => Promise.resolve(updatedEvent),
+  deleteEvent: (eventId: number) => Promise.resolve({} as Event),
+});
+
+const BASE_URL = "http://localhost:3000/api/event/";
+
+export const EventProvider = ({ children }: EventContextProviderProps) => {
+  const [events, setEvents] = useState<Event[]>([]);
+
+  const getAllEvents = async () => {
+    try {
+      const response = await axios.get(BASE_URL);
+      setEvents(response.data);
+    } catch (error: any) {
+      throw error.response.statusText;
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      await getAllEvents();
+    })();
+  }, []);
+
+  const createEvent = async (newEvent: newEvent) => {
+    try {
+      const response = await axios.post(BASE_URL, newEvent);
+      await getAllEvents();
+      return response.data;
+    } catch (error: any) {
+      throw error.response.statusText;
+    }
+  };
+
+  const getEvent = async (eventId: number) => {
+    const eventIdURL = `${BASE_URL}${eventId}`;
+    try {
+      const response = await axios.get(eventIdURL);
+      return response.data;
+    } catch (error: any) {
+      throw error.response.statusText;
+    }
+  };
+
+  const updateEvent = async (updatedEvent: Event) => {
+    const eventIdURL = `${BASE_URL}${updatedEvent.eventId}`;
+    try {
+      const response = await axios.put(eventIdURL, updatedEvent);
+      await getAllEvents();
+      return response.data;
+    } catch (error: any) {
+      throw error.response.statusText;
+    }
+  };
+
+  const deleteEvent = async (eventId: number) => {
+    const eventIdURL = `${BASE_URL}${eventId}`;
+    try {
+      const response = await axios.delete(eventIdURL);
+      await getAllEvents();
+      return response.data;
+    } catch (error: any) {
+      throw error.response.statusText;
+    }
+  };
+
+  return (
+    <EventContext.Provider
+      value={{
+        events,
+        setEvents,
+        getAllEvents,
+        createEvent,
+        getEvent,
+        updateEvent,
+        deleteEvent,
+      }}
+    >
+      {children}
+    </EventContext.Provider>
+  );
+};
